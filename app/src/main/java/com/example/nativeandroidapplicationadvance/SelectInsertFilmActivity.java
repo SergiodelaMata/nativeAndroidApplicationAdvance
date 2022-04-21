32326c339ae1626b9288a7235512dda6bb7bee7a
SergiodelaMata <sergio.matam@edu.uah.es>
2022-04-21 12:26:02 +0200

    :sparkles: Added activities to search a film to be added and to insert the data from the date in which it was seen.

---
+++ app/src/main/java/com/example/nativeandroidapplicationadvance/SelectInsertFilmActivity.java
@@ -0,0 +1,14 @@
+package com.example.nativeandroidapplicationadvance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectInsertFilmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_insert_film);
+    }
+}
